f590d9e7c68e30c8bb27f87c55ad3bf9746db7a2
Vladislav Putyrskiy <V.V.Putyrskiy@gmail.com>
2022-08-08 23:40:05 +0300

    1st

---
+++ 20.2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <ctime>
+
+int main()
+{   
+    std::srand(std::time(nullptr));
+    int letter;
+    std::string a, b;
+    std::stringstream line;
+    int width = 0;
+    int high = 0;
+    std::cout << "input the width and the high\n";
+    std::cin >> a >> b;
+    try
+    {
+        width = stoi(a);
+        high = stoi(b);
+    }
+    catch (std::exception& err)
+    {
+        std::cout<<"incorrect input";
+        return 0;
+    }
+    for (int i = 0; i < high; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {   
+            letter = rand()%2;
+            line << letter;
+        }
+        line << std::endl;
+    }
+    std::ofstream file("figure.txt");
+    if (file.is_open())
+    {
+        file << line.str();
+    }
+    else
+    {
+        std::cout << "something wrong with file";
+    }
+}
+
